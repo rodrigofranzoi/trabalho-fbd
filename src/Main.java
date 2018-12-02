c551be63488f226ba2d5dd2ed78adefc4e0bb470
rodrigofranzoi <rodrigo.scroferneker@gmail.com>
2018-12-02 15:50:04 -0200

    main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+    private static final long serialVersionUID = 1L;
+    public static void main(String[] args) {
+
+
+
+    }
+}
